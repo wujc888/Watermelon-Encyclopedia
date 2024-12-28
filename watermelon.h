063fc3c9934ec56ed47efbcc0d365dcd5d02cdae
西瓜小吴 <xi_gua888@126.com>
2024-12-28 18:38:07 +0800

    Create watermelon.h
    添加自定义库

---
+++ watermelon.h
@@ -0,0 +1,26 @@
+#ifndef WATERMELON_H
+#define WATERMELON_H
+
+#include <iostream>
+#include <string>
+
+class Watermelon {
+public:
+    void printTypes();
+    void set();
+    void hide();
+    void show();
+    void getnew();
+    void printNutrition();
+    void printCultivation();
+    void funFact();
+    void picture();
+    void where();
+    void zhishi();
+    void migong();
+    void hit();
+    void shengming();
+    void pwt();
+};
+
+#endif // WATERMELON_H
